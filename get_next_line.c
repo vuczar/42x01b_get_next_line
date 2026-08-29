b451bfa4786a99b3588e06a5727a44ae847b0564
noone <boringmkt@gmail.com>
2026-08-29 09:45:35 -0300

    started

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,17 @@
 /*   By: vimauric <vimauric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 19:53:32 by vimauric          #+#    #+#             */
-/*   Updated: 2026/08/26 19:54:39 by vimauric         ###   ########.fr       */
+/*   Updated: 2026/08/29 09:28:54 by vimauric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*fill_stash(int fd, char *stash);
+
+char	*extract_line(char *stash);
+
+char	*advance_stash(char *stash);
+
 char	*get_next_line(int fd)
 {
-	
+	static char	*stash;
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,12 @@
 /*   By: vimauric <vimauric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 19:55:57 by vimauric          #+#    #+#             */
-/*   Updated: 2026/08/26 19:55:58 by vimauric         ###   ########.fr       */
+/*   Updated: 2026/08/29 09:24:08 by vimauric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+size_t	ft_strlen(const char *s);
+
+char	*ft_strchr(const char *s, int c);
+
+char	*ft_strjoin_free(char *s1, char *s2);
